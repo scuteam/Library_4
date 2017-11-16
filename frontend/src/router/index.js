6ae4735a718d23496b4ede1fc8953518c21d04db
zephania <zephaniacn@outlook.com>
2017-11-16 19:49:55 +0800

    init front end

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,32 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Borrow from '@/components/views/Borrow'
-import Login from '@/components/views/Login'
-import Renew from '@/components/views/Renew'
-import BookManage from '@/components/views/BookManage'
+import HelloWorld from '@/components/HelloWorld'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Borrow',
-      component: Borrow
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/renew',
-      name: 'Renew',
-      component: Renew
-    },
-    {
-      path: '/book_manage',
-      name: 'BookManage',
-      component: BookManage
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
